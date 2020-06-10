4bc3ab61a68215bd26a6013905687a87105a7c81
kenkeeeenbbt <kenkeeeenbbt@gmail.com>
2020-06-10 00:17:06 +0000

    一意性の検証

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -67,6 +67,22 @@ RSpec.describe User, type: :model do
         end
     end
 
-    describe '' do
+    describe '一意性の検証' do
+        it 'メールアドレスが小文字化されていること' do
+            @user.email = "TEST@EXAMPLE.COM"
+            @user.save
+            expect(@user.email).to eq("test@example.com")
+        end
+
+        it '重複したメールアドレスの場合、無効であること' do
+            user1 = create(:user, name: 'taro', email: 'taro@example.com')
+            user2 = build(:user, name: 'jiro', email: user1.email)
+            expect(user2.valid?).to eq(false)
+        end
+    end
+
+    describe 'パスワードの検証' do
+        it 'パスワードと確認用パスワードが間違っている場合、無効であること' do
+        end
     end
 end
